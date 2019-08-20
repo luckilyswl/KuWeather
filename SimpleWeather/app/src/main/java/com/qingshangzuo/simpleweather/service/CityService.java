f96cf27dbb322b5682bdaa2d4f7ea42c20d258d5
luckilyswl <792411500@qq.com>
2019-08-20 14:07:52 +0800

    添加一个新的天气程序，可以运行，但是没有数据显示，自己认为是版本/依赖的问题

---
+++ SimpleWeather/app/src/main/java/com/qingshangzuo/simpleweather/service/CityService.java
@@ -0,0 +1,24 @@
+package com.qingshangzuo.simpleweather.service;
+
+
+
+import com.qingshangzuo.simpleweather.json.CityBean;
+
+
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface CityService {
+
+    @GET("find")
+    Observable<CityBean> getCall(
+
+            @Query("location")
+            String location,
+
+            @Query("key")
+            String key
+    );
+}
---
+++ SimpleWeather/app/src/main/java/com/qingshangzuo/simpleweather/service/WeatherService.java
@@ -0,0 +1,17 @@
+package com.qingshangzuo.simpleweather.service;
+
+
+import com.qingshangzuo.simpleweather.json.WeatherBean;
+
+import io.reactivex.Observable;
+import okhttp3.ResponseBody;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface WeatherService {
+    @GET("{city_code}")
+    Observable<WeatherBean> getCall(@Path("city_code") String code);
+
+    @GET("https://cdn.heweather.com/cond_icon/{picture_id}")
+    Observable<ResponseBody> getPicture(@Path("picture_id") String id);
+}
